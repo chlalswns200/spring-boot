0a8d88c0b6fbbb8805972bc810a8a4673a1a26dd
chlalswns200 <chlalswns200@naver.com>
2022-10-27 11:47:33 +0900

    QueryParam map

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
@@ -25,6 +27,14 @@ public class HelloController {
     @GetMapping(value = "/request1")
     public String getVariable2(@RequestParam String name,@RequestParam String email,@RequestParam String organization) {
         return String.format("%s %s %s",name,email,organization);
+    }
+
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String,String> param) {
+        param.entrySet().forEach((map)->{
+            System.out.printf("key:%s value:%s\n",map.getKey(),map.getValue());
+        });
+        return "호출 완료 되었습니다.";
     }
 
 
