757630d497c98ca3ae20b9d8db8ffc414e181548
chlalswns200 <chlalswns200@naver.com>
2022-10-27 14:36:13 +0900

    Put

---
+++ src/main/java/com/example/hello/controller/PutController.java
@@ -0,0 +1,23 @@
+package com.example.hello.controller;
+
+
+import com.example.hello.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+
+
+}
