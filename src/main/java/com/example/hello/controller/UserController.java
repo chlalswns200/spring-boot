997ce0d26cbbfe75cf15cea58a2500aca019c1ec
chlalswns200 <chlalswns200@naver.com>
2022-10-28 14:14:28 +0900

    add 추가

--- src/main/java/com/example/hello/controller/UserController.java
+++ src/main/java/com/example/hello/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
 import com.example.hello.dao.UserDao;
+import com.example.hello.domain.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ public class UserController {
 
     public UserController(UserDao userDao) {
         this.userDao = userDao;
+    } // Could not autowire. No beans of "UserDao" type found ??
+
+    @PostMapping("sign")
+    public void addUser(@RequestBody User user) {
+        UserDao.add(user);
     }
 }
