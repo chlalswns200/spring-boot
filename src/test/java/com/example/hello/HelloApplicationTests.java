6bd9ea387f7204aeeb7e1d6c4c9c11b401720f5a
chlalswns200 <chlalswns200@naver.com>
2022-10-31 14:26:39 +0900

    Parser, 테스트코드 '통과'

--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -21,22 +21,22 @@ class HelloApplicationTests {
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
 
-        assertEquals(1, hospital.getId());
-        assertEquals("의원", hospital.getOpenServiceName());
-        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
-        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
-        assertEquals(LocalDateTime.of(1999,6,12,0,0,0), hospital.getLicenseDate());
-        assertEquals(1, hospital.getBusinessStatus());
-        assertEquals(13, hospital.getBusinessStatusCode());
-        assertEquals("062-515-2875", hospital.getPhone());
-        assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
-        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());
-        assertEquals("효치과의원", hospital.getHospitalName());
-        assertEquals("치과의원", hospital.getBusinessTypeName());
-        assertEquals(1, hospital.getHealthcareProviderCount());
-        assertEquals(0, hospital.getPatientRoomCount());
-        assertEquals(0, hospital.getTotalNumberOfBeds());
-        assertEquals(52.29f, hospital.getTotalAreaSize());
+        assertEquals(1, hospital.getId()); // col:0
+        assertEquals("의원", hospital.getOpenServiceName());//col:1
+        assertEquals(3620000,hospital.getOpenLocalGovernmentCode()); // col: 3
+        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber()); // col:4
+        assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); //19990612 //col:5
+        assertEquals(1, hospital.getBusinessStatus()); //col:7
+        assertEquals(13, hospital.getBusinessStatusCode());//col:9
+        assertEquals("062-515-2875", hospital.getPhone());//col:15
+        assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress()); //col:18
+        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());//col:19
+        assertEquals("효치과의원", hospital.getHospitalName());//col:21
+        assertEquals("치과의원", hospital.getBusinessTypeName());//col:25
+        assertEquals(1, hospital.getHealthcareProviderCount()); //col:29
+        assertEquals(0, hospital.getPatientRoomCount()); //col:30
+        assertEquals(0, hospital.getTotalNumberOfBeds()); //col:31
+        assertEquals(52.29f, hospital.getTotalAreaSize()); //col:32
 
     }
 
